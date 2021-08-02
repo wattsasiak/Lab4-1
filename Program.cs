bba58bc4e2f17a43258431c330449d1741ac9933
Asia Watts <AsiaWatts@quickenloans.com>
2021-08-02 17:05:43 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+
+namespace Lab4_1
+{
+
+	class Book
+    {
+
+		public string AuthFirstName;
+		public string AuthLastName;
+		public string ID;
+		public string Publisher;
+		public int CopiesSold;
+
+
+		public void Sell(int BooksSold)
+        {
+			CopiesSold =  CopiesSold + BooksSold;
+			
+        }
+
+
+		public void Print()
+        {
+
+			Console.WriteLine("Book Information: ");
+			Console.WriteLine($"Author Name: {AuthFirstName} {AuthLastName}");
+			Console.WriteLine($"ISBN: {ID}");
+			Console.WriteLine($"Publisher: {Publisher}");
+			Console.WriteLine($"Number of Copies Sold: {CopiesSold}");
+
+		}
+    }
+
+
+
+
+
+
+	class Parallelogram
+	{
+		public double Length;
+		public double Width;
+		
+		
+		public double GetArea()
+		{
+
+			return Length * Width;
+			
+		}
+
+
+		public double GetPerimeter()
+        {
+			return (Length * 2)+ (Width * 2);
+        }
+
+		public void Print()
+        {
+			Console.WriteLine("Here is your Parallelogram!");
+			Console.WriteLine($"Area: {GetArea()}");
+			Console.WriteLine($"Perimeter: {GetPerimeter()}");
+
+        }
+
+
+		public void Resize (double newLength, double newWidth)
+        {
+
+			if (newLength < 0 | newWidth < 0)
+            {
+				return;
+            }
+
+
+
+
+			Length = newLength;
+			Width = newWidth;
+        }
+
+
+
+	}
+
+	class Program
+    {
+		static void Main(string[] args)
+        {
+
+			Parallelogram p1 = new Parallelogram() { Length = 5, Width = 6 };
+			p1.Print();
+
+
+			Parallelogram p2 = new Parallelogram() { Length = 25, Width = 60 };
+			p2.Print();
+
+
+			p1.Resize(5, 10);
+			p1.Print();
+
+
+			Console.WriteLine("Book 1: ");
+			Book book1 = new Book() { AuthFirstName = "Jane", AuthLastName = "Smith", CopiesSold = 600, ID = "asdj259789", Publisher = "Smith Publishing Co." };
+			book1.Print();
+
+
+
+
+
+
+			Console.WriteLine("Book 2: ");
+			Book book2 = new Book() { AuthFirstName = "John", AuthLastName = "Simmons", CopiesSold = 825, ID = "gdksg2e482t5", Publisher = "Sunny publishing" };
+			book2.Print();
+
+
+			Console.WriteLine(" Book 1 updated copies sold: ");
+
+			book1.Sell(5);
+			book1.Print();
+
+
+			Console.WriteLine(" Book 2 updated copies sold: ");
+
+			book1.Sell(10);
+			book1.Print();
+		}
+	}
+
+
+
+}
