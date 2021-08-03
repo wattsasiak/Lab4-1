3891f76899429698bb082e49f5e919cf2f19043e
Asia Watts <AsiaWatts@quickenloans.com>
2021-08-03 09:23:22 -0400

    added while loop

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@
 namespace Lab4_1
 {
 
+
+
+
 	class Book
     {
 
@@ -24,7 +27,7 @@ namespace Lab4_1
 		public void Print()
         {
 
-			Console.WriteLine("Book Information: ");
+
 			Console.WriteLine($"Author Name: {AuthFirstName} {AuthLastName}");
 			Console.WriteLine($"ISBN: {ID}");
 			Console.WriteLine($"Publisher: {Publisher}");
@@ -86,46 +89,77 @@ namespace Lab4_1
 	}
 
 	class Program
-    {
+	{
+		static bool Continue()
+		{
+			while (true)
+			{
+				Console.WriteLine("Would you like to continue? (y/n)");
+				string response = Console.ReadLine();
+				response = response.ToLower();
+
+
+				if (response == "y" || response == "yes")
+				{
+					return true;
+				}
+				else if (response == "n" || response == "no")
+				{
+					return false;
+				}
+				else
+				{
+					Console.WriteLine("Please enter y or n");
+				}
+
+			}
+		}
+
+
 		static void Main(string[] args)
-        {
-
-			Parallelogram p1 = new Parallelogram() { Length = 5, Width = 6 };
-			p1.Print();
-
-
-			Parallelogram p2 = new Parallelogram() { Length = 25, Width = 60 };
-			p2.Print();
+		{
+			do
+			{
 
 
-			p1.Resize(5, 10);
-			p1.Print();
+				Parallelogram p1 = new Parallelogram() { Length = 5, Width = 6 };
+				p1.Print();
+				Console.WriteLine();
+
+				Parallelogram p2 = new Parallelogram() { Length = 25, Width = 60 };
+				p2.Print();
+				Console.WriteLine();
+
+				p1.Resize(5, 10);
+				p1.Print();
+				Console.WriteLine();
+
+				Console.WriteLine("Book 1: ");
+				Book book1 = new Book() { AuthFirstName = "Jane", AuthLastName = "Smith", CopiesSold = 600, ID = "asdj259789", Publisher = "Smith Publishing Co." };
+				book1.Print();
+				Console.WriteLine();
 
 
-			Console.WriteLine("Book 1: ");
-			Book book1 = new Book() { AuthFirstName = "Jane", AuthLastName = "Smith", CopiesSold = 600, ID = "asdj259789", Publisher = "Smith Publishing Co." };
-			book1.Print();
 
 
 
+				Console.WriteLine("Book 2: ");
+				Book book2 = new Book() { AuthFirstName = "John", AuthLastName = "Simmons", CopiesSold = 825, ID = "gdksg2e482t5", Publisher = "Sunny publishing" };
+				book2.Print();
+				Console.WriteLine();
 
+				Console.WriteLine(" Book 1 updated copies sold: ");
 
+				book1.Sell(5);
+				book1.Print();
+				Console.WriteLine();
 
-			Console.WriteLine("Book 2: ");
-			Book book2 = new Book() { AuthFirstName = "John", AuthLastName = "Simmons", CopiesSold = 825, ID = "gdksg2e482t5", Publisher = "Sunny publishing" };
-			book2.Print();
+				Console.WriteLine(" Book 2 updated copies sold: ");
 
-
-			Console.WriteLine(" Book 1 updated copies sold: ");
-
-			book1.Sell(5);
-			book1.Print();
-
-
-			Console.WriteLine(" Book 2 updated copies sold: ");
-
-			book1.Sell(10);
-			book1.Print();
+				book1.Sell(10);
+				book1.Print();
+				Console.WriteLine();
+			} while (Continue());
 		}
 	}
 
